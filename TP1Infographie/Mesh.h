2e45e50c9b99fd37fdda6b6937bb2475d02fd8f3
Silendel <greg.badin@gmail.com>
2014-03-21 09:51:05 +0100

    Modification des chemin pour virer l'absolut et mettre du relatif + ajut des classes qu'il manquais

---
+++ TP1Infographie/Mesh.h
@@ -0,0 +1,36 @@
+#ifndef MESH
+#define MESH
+
+#include <GL/glew.h>
+#include <fstream>
+#include <sstream>
+#include <iostream>
+#include <SDL.h>
+#include <string>
+#include <sstream>
+
+class Mesh{
+	private:
+		GLuint m_ID;
+		unsigned int m_numFaces;
+		unsigned int m_numVertices;
+		GLfloat *m_vertices;
+		GLuint *m_faces;
+
+	public:
+		Mesh();
+		~Mesh();
+
+		void init(GLuint numVerts, GLfloat* vertices, GLfloat* colours, GLfloat* normals, GLfloat* texcoords, GLuint indexCount, GLuint* indices);
+		void init(GLuint numVerts, GLfloat* vertices, GLfloat* colours, GLfloat* normals,GLfloat* texcoords);
+		void init(GLuint numVerts, GLfloat* vertices);
+		void loadFromObjFile(const char* filename);
+		void draw();
+
+		unsigned int getNumFaces();
+		unsigned int getNumVertices();
+		float *getVertices();
+		unsigned int *getFaces();
+		GLuint getID();
+};
+#endif
